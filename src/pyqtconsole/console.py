a47c31bb0c24cf05273c426ec5a0cb13a224d578
Jacob Williams <jacobwilliams@users.noreply.github.com>
2026-02-19 22:25:05 -0600

    add highlighting to shell cmd lines
    Fixes #111

--- src/pyqtconsole/console.py
+++ src/pyqtconsole/console.py
@@ -699,7 +699,8 @@ class PythonConsole(BaseConsole):
             outprompt=outprompt
         )
         self.highlighter = PythonHighlighter(
-            self.edit.document(), formats=formats)
+            self.edit.document(), formats=formats,
+            shell_cmd_prefix=self.shell_cmd_prefix)
         self.interpreter = PythonInterpreter(
             self.stdin, self.stdout, locals=locals)
         self.interpreter.done_signal.connect(self._finish_command)
